544c7d8976db7a845172687255857d83a9eb9877
nrwrn <nrwrn@protonmail.com>
2021-12-13 23:38:58 -0500

    ... with tests

--- tests/test_aoc.py
+++ tests/test_aoc.py
@@ -23,3 +23,23 @@ def test_d5_p2():
     file = resources.open_text(input, 'day5.txt')
     result = get_func(5, 2)([file])
     assert result == 12 
+
+def test_d6_p1():
+    file = resources.open_text(input, 'day6.txt')
+    result = get_func(6, 1)([file])
+    assert result == 5934
+
+def test_d6_p2():
+    file = resources.open_text(input, 'day6.txt')
+    result = get_func(6, 2)([file])
+    assert result == 26984457539
+
+def test_d7_p1():
+    file = resources.open_text(input, 'day7.txt')
+    result = get_func(7, 1)([file])
+    assert result == 37
+
+def test_d7_p2():
+    file = resources.open_text(input, 'day7.txt')
+    result = get_func(7, 2)([file])
+    assert result == 168
