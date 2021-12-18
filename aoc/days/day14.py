5ab860ce8cfc7c54af299b0e299351602e2de035
nrwrn <nrwrn@protonmail.com>
2021-12-18 09:30:08 -0500

    day 14 part 1 rewritten to use recursive memoized

--- aoc/days/day14.py
+++ aoc/days/day14.py
@@ -10,22 +10,6 @@ def get_rules(lines: TextIOBase) -> Dict[str, str]:
     return rules
 
 
-def apply(rules: Dict[str, str], polymer: str):
-    insertions: List[Optional[str]] = [None for _ in range(len(polymer) - 1)]
-    for i in range(len(polymer) - 1):
-        pair = polymer[i : i + 2]
-        if pair in rules:
-            insertions[i] = rules[pair]
-    offset = 1
-    li = list(polymer)
-    for i, insert in enumerate(insertions):
-        if insert is None:
-            continue
-        li.insert(i + offset, insert)
-        offset += 1
-    return "".join(li)
-
-
 def histogram(s: str) -> Dict[str, int]:
     h = {}
     for char in s:
@@ -78,12 +62,10 @@ class Polymer:
 
 def part1(files: List[TextIOBase]) -> int:
     lines = files[0]
-    polymer = lines.readline().strip()
+    seed = lines.readline().strip()
     lines.readline()  # empty line
     rules = get_rules(lines)
-    for _ in range(10):
-        polymer = apply(rules, polymer)
-    freqs = histogram(polymer).values()
+    freqs = Polymer(seed, rules).run(10).values()
     return max(freqs) - min(freqs)
 
 
@@ -92,6 +74,5 @@ def part2(files: List[TextIOBase]) -> int:
     seed = lines.readline().strip()
     lines.readline()  # empty line
     rules = get_rules(lines)
-    polymer = Polymer(seed, rules)
-    freqs = polymer.run(40).values()
+    freqs = Polymer(seed, rules).run(40).values()
     return max(freqs) - min(freqs)
